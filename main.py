37b87df603f773157dae13d139f355d6949f0799
Carlos Fuerte <113551817+CursedCola@users.noreply.github.com>
2022-09-27 19:42:33 +0000

    after review

--- main.py
+++ main.py
@@ -11,17 +11,19 @@
 # review practice
 # Append the value of current to the end of the list seconds Please use the list.append() method to do that.
 
+# seconds = [1.23, 1.45, 1.02]
+# seconds.append(1.11)
+# print(seconds)
 
-seconds = [1.23, 1.45, 1.02]
-current = 1.11
+# # Remove item 1.45 from seconds.
+# seconds = [1.23, 1.45, 1.02, 1.11]
+# seconds.remove(1.45)
+# print(seconds)
 
-# Remove item 1.45 from seconds.
-seconds = [1.23, 1.45, 1.02, 1.11]
-
-
-# Remove items 1.45, 1.02, and 1.11 from seconds.
-seconds = [1.23, 1.45, 1.02, 1.11]
-
+# # Remove items 1.45, 1.02, and 1.11 from seconds.
+# seconds = [1.23, 1.45, 1.02, 1.11]
+# newSeconds = seconds[1:]
+# print(newSeconds)
 
 ################################comparison operators#########################
 #remember....
